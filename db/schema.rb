8b9cdbd3f21e0647608450c44cabc3aa3525971c
tania-macheieva <macheieva.tetiana@gmail.com>
2025-05-30 20:14:45 +0000

    feat: create the db table(question)

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_30_183255) do
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 end
