95a806aefd6e1a1d979df4b57ce43bb3949f351b
tania-macheieva <macheieva.tetiana@gmail.com>
2025-06-03 18:07:36 +0000

    fix: fix trailing whitespace in migration file

--- db/migrate/20250530183255_create_questions.rb
+++ db/migrate/20250530183255_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[8.0]
   def change
     create_table :questions do |t|
       t.text :body
-      t.integer :user_id, index: true 
+      t.integer :user_id, index: true
 
       t.timestamps
     end
