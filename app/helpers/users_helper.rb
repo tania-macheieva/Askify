9723fd259a2260982d28d1df2bc16b80cdeb280d
tania-macheieva <macheieva.tetiana@gmail.com>
2025-06-11 20:27:33 +0000

    fix: fix XSS vulnerability in social links|final fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,30 @@
 module UsersHelper
+  def safe_external_link(url, options = {})
+    return nil if url.blank?
+
+    begin
+      uri = URI.parse(url)
+      return nil unless %w[http https].include?(uri.scheme)
+
+      uri.to_s
+    rescue URI::InvalidURIError
+      nil
+    end
+  end
+
+  def safe_github_url(url)
+    safe_url = safe_external_link(url)
+    return nil unless safe_url
+
+    uri = URI.parse(safe_url)
+    uri.host&.downcase&.end_with?("github.com") ? safe_url : nil
+  end
+
+  def safe_linkedin_url(url)
+    safe_url = safe_external_link(url)
+    return nil unless safe_url
+
+    uri = URI.parse(safe_url)
+    uri.host&.downcase&.include?("linkedin.com") ? safe_url : nil
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,38 @@ class User < ApplicationRecord
   before_save :downcase_nickname
 
   validates :email, presence: true, uniqueness: true
-  validates :github_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
-  validates :linkedin_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
+
+  validates :github_url, :linkedin_url, format: {
+    with: /\Ahttps?:\/\/.+\z/,
+    message: "must be a valid HTTP or HTTPS URL"
+  }, allow_blank: true
+
+  validate :validate_github_url, if: -> { github_url.present? }
+  validate :validate_linkedin_url, if: -> { linkedin_url.present? }
 
   def downcase_nickname
     nickname.downcase!
+  end
+
+  private
+
+  def validate_github_url
+    return unless github_url.present?
+    uri = URI.parse(github_url)
+    unless uri.host&.downcase&.end_with?("github.com")
+      errors.add(:github_url, "must be a GitHub URL")
+    end
+  rescue URI::InvalidURIError
+    errors.add(:github_url, "must be a valid URL")
+  end
+
+  def validate_linkedin_url
+    return unless linkedin_url.present?
+    uri = URI.parse(linkedin_url)
+    unless uri.host&.downcase&.include?("linkedin.com")
+      errors.add(:linkedin_url, "must be a LinkedIn URL")
+    end
+  rescue URI::InvalidURIError
+    errors.add(:linkedin_url, "must be a valid URL")
   end
 end
