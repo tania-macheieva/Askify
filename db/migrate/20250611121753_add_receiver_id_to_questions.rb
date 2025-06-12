76caeab36a962101eaf5724a83246a4c7c60a85e
tania-macheieva <macheieva.tetiana@gmail.com>
2025-06-12 19:38:22 +0000

    feat: add homepage with user cards, improve styles and refactor code

--- db/migrate/20250611121753_add_receiver_id_to_questions.rb
+++ db/migrate/20250611121753_add_receiver_id_to_questions.rb
@@ -1,4 +1,4 @@
-class AddReceiverIdToQuestions < ActiveRecord::Migration[8.0]
+class AddReceiverIdToQuestions < ActiveRecord::Migration[6.0]
   def change
     add_column :questions, :receiver_id, :integer
   end
